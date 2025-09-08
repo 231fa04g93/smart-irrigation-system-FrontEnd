0e133337226941fc45c955d408e1d66616613b74
231fa04g93 <231fa04g93@gmail.com>
2025-09-08 20:30:18 +0530

    Deploy Vite build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
    base: "/smart-irrigation-system-FrontEnd/",
+=======
+base: "/smart-irrigation-system-FrontEnd/",
+>>>>>>> fd242fe (Deploy Vite build)
 })
