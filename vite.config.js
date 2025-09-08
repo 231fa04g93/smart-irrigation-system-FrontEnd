819abe44d8f2946ef80d03aad0a8766753f3a20b
231fa04g93 <231fa04g93@gmail.com>
2025-09-08 20:42:54 +0530

    Fix: Corrected base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/your-repo-name/', // Make sure this is correct
+  base: '/smart-irrigation-system-FrontEnd/', 
   plugins: [react()],
   build: {
-    outDir: 'docs', // Add this line
+    outDir: 'docs',
   },
 });
